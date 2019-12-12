245971c7535f09f9277cf80539cc3c3d3d939cc4
o-marvel <52745865+o-marvel@users.noreply.github.com>
2019-12-12 11:00:26 +0100

    first trial fro two column profile

---
+++ rough.js
@@ -0,0 +1,42 @@
+
+var card = {
+    name : "spade",
+    id : 50,
+};
+console.log(card.name);
+//  array of objects 
+let  cars = [
+    { 
+        brand: " toyota",
+        color: "red",
+    },
+    { 
+        brand: " volvo",
+        color: 'blue',
+    },
+    {
+        brand: " dodge",
+        color: 'green',
+    }
+];
+//  console.log(cars[0].brand);
+//   let result = Math.random();
+//   console.log(result);
+//   let timezone = new Date().toDateString();
+//   console.log(timezone);
+let textArea = document.querySelector('.menu');
+textArea.innerHTML = 'hello world';
+String.prototype.Hello = function () {
+ return this. toString() + ' heloo';  
+} 
+
+console.log(JSON.stringify(cars));
+
+
+
+  
+
+  
+
+
+ 
